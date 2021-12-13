b6c45caa3e2e8a98d50568766e75bfe31ce0255c
HlaingBwarAg <91529726+HlaingBwarAg@users.noreply.github.com>
2021-12-13 23:48:29 +0630

    day 1

---
+++ time.js
@@ -0,0 +1,46 @@
+// let days = document.getElementById("days")
+
+// let year = document.getElementById("year")
+// let month = document.getElementById("month")
+// let day = document.getElementById("day")
+// let hour = document.getElementById("hour")
+// let minute = document.getElementById("minute")
+// let second = document.getElementById("second")
+
+// const eventDate = new Date("2021-02-21").getTime()
+// const now = new Date().getTime()
+// console.log( eventDate )
+// console.log( now )
+
+// const gap = now - eventDate
+// console.log(gap)
+
+// const second = 1000;
+// const minute = second * 60;
+// const hour = minute * 60;
+// const day = hour * 24;
+ 
+
+// const textDays = Math.floor(gap / day)
+// const textYear = Math.floor((gap / day) % 365)
+
+// const  = Math.floor(gap / day)
+// const  = Math.floor(gap / day)
+// const  = Math.floor(gap / day)
+// const  = Math.floor(gap / day)
+// const  = Math.floor(gap / day)
+
+const eventDate = new Date("2020-02-21")
+const now = new Date()
+
+// year
+year = now.getFullYear() - eventDate.getFullYear() 
+console.log(year)
+
+// month
+month = now.getMonth() - eventDate.getMonth()
+console.log(month)
+
+// day
+day = now.getDate() - eventDate.getDate()
+console.log(day)
